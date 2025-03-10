2758e468b3bc4ea2011bccf14eb3d82a9def77f6
mig23ang <miguel.reyesc@usantoto.edu.co>
2025-03-10 10:30:10 -0500

    Refactor: se crea los repositorys de usuario

---
+++ src/main/java/registro/samtel/horas/repository/models/UsuarioEntity.java
@@ -0,0 +1,25 @@
+package registro.samtel.horas.repository.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    private  String nombre;
+
+
+
+}
