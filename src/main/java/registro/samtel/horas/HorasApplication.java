21a4aa005291c5fb6fd29336f42a622a8e86806b
mig23ang <miguel.reyesc@usantoto.edu.co>
2025-03-10 09:46:22 -0500

    carga inicial

---
+++ src/main/java/registro/samtel/horas/HorasApplication.java
@@ -0,0 +1,13 @@
+package registro.samtel.horas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/registro/samtel/horas/HorasApplicationTests.java
@@ -0,0 +1,13 @@
+package registro.samtel.horas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
