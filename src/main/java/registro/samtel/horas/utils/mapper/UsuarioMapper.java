f6e9195760796acd87ab91e1339cade22dc915f2
mig23ang <miguel.reyesc@usantoto.edu.co>
2025-04-02 08:50:19 -0500

    ajuste modelmapper

--- src/main/java/registro/samtel/horas/utils/mapper/UsuarioMapper.java
+++ src/main/java/registro/samtel/horas/utils/mapper/UsuarioMapper.java
@@ -1,14 +1,23 @@
 package registro.samtel.horas.utils.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import registro.samtel.horas.models.entities.UsuarioEntity;
 
-import java.util.logging.Logger;
-
+@Component
 public class UsuarioMapper {
-    private static Logger log = Logger.getLogger(String.valueOf(UsuarioMapper.class));
 
-    public UsuarioEntity usuarioToUsuarioEntity(usuario) {
-        return null;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    // Convertir DTO a Entidad
+    public UsuarioEntity usuarioDTOToUsuarioEntity(UsuarioEntity usuarioDTO) {
+        return modelMapper.map(usuarioDTO, UsuarioEntity.class);
     }
 
+    // Convertir Entidad a DTO
+    public UsuarioEntity usuarioEntityToUsuarioDTO(UsuarioEntity usuarioEntity) {
+        return modelMapper.map(usuarioEntity, UsuarioEntity.class);
+    }
 }
