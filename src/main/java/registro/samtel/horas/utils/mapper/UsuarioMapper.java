e238d6b2250783e117a2a087334b6f714a12aa7f
mig23ang <miguel.reyesc@usantoto.edu.co>
2025-04-02 08:50:59 -0500

    ajuste modelmapper

--- src/main/java/registro/samtel/horas/utils/mapper/UsuarioMapper.java
+++ src/main/java/registro/samtel/horas/utils/mapper/UsuarioMapper.java
@@ -1,23 +1,23 @@
-package registro.samtel.horas.utils.mapper;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import registro.samtel.horas.models.entities.UsuarioEntity;
-
-@Component
-public class UsuarioMapper {
-
-    @Autowired
-    private ModelMapper modelMapper;
-
-    // Convertir DTO a Entidad
-    public UsuarioEntity usuarioDTOToUsuarioEntity(UsuarioEntity usuarioDTO) {
-        return modelMapper.map(usuarioDTO, UsuarioEntity.class);
-    }
-
-    // Convertir Entidad a DTO
-    public UsuarioEntity usuarioEntityToUsuarioDTO(UsuarioEntity usuarioEntity) {
-        return modelMapper.map(usuarioEntity, UsuarioEntity.class);
-    }
-}
+//package registro.samtel.horas.utils.mapper;
+//
+//import org.modelmapper.ModelMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import registro.samtel.horas.models.entities.UsuarioEntity;
+//
+//@Component
+//public class UsuarioMapper {
+//
+//    @Autowired
+//    private ModelMapper modelMapper;
+//
+//    // Convertir DTO a Entidad
+//    public UsuarioEntity usuarioDTOToUsuarioEntity(UsuarioEntity usuarioDTO) {
+//        return modelMapper.map(usuarioDTO, UsuarioEntity.class);
+//    }
+//
+//    // Convertir Entidad a DTO
+//    public UsuarioEntity usuarioEntityToUsuarioDTO(UsuarioEntity usuarioEntity) {
+//        return modelMapper.map(usuarioEntity, UsuarioEntity.class);
+//    }
+//}
