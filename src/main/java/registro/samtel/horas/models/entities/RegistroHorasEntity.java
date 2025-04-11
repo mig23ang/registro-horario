3661c6f5e2396776625c716014db581c45e69f5d
Fernando Alonso Martinez Rios <fernando.martinez@samtel.local>
2025-04-11 17:35:44 -0500

    aca agrege algunas validaciones pero hay voy

--- src/main/java/registro/samtel/horas/models/entities/RegistroHorasEntity.java
+++ src/main/java/registro/samtel/horas/models/entities/RegistroHorasEntity.java
@@ -2,9 +2,12 @@ package registro.samtel.horas.models.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.time.LocalDate;
 
@@ -19,20 +22,28 @@ public class RegistroHorasEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private LocalDate fechaDeRegistro;
+    @NotBlank
 
-    @Column(nullable = false)
+    private String fechaDeRegistro;
+
+    @Pattern(
+            regexp = "^(1[0-2]|0?[1-9])\\s?(am|pm)$",
+            message = "La hora debe estar en formato 12 horas, como '1 am' o '12 pm'"
+    )
+
     private String horaEntrada;
-
-    @Column(nullable = true)
+    @NotBlank
+    @Pattern(
+            regexp = "^(1[0-2]|0?[1-9])\\s?(am|pm)$",
+            message = "La hora debe estar en formato 12 horas, como '1 am' o '12 pm'"
+    )
     private String horaSalida;
 
-    @Column(nullable = false)
+
     private Boolean estadoRegistro;
 
     @ManyToOne
-    @JoinColumn(name = "usuario_id", nullable = false)
+    @JoinColumn(name = "usuario_id")
     private UsuarioEntity usuario;
 
 }
--- src/main/java/registro/samtel/horas/models/entities/UsuarioEntity.java
+++ src/main/java/registro/samtel/horas/models/entities/UsuarioEntity.java
@@ -2,6 +2,9 @@ package registro.samtel.horas.models.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,15 +23,21 @@ public class UsuarioEntity {
     private  Long id;
 
     @Column(nullable = false, length = 50)
+    @Size(min = 3,max= 12, message = "El nombre es necesario")
+    @NotBlank(message = "solo su primer nombre")
+
     private  String nombre;
 
     @Column(nullable = false, length = 50, unique = true)
+    @Email(message = "El correo deve ser valido")
+    @NotBlank(message = "Correo de la empresa")
     private  String correo;
 
     @Enumerated(EnumType.STRING) // Guardar enum como String en la base de datos
     @Column(nullable = false)
     private RolUsuario rol;
 
+    @NotBlank
     private String password;
 
     @Column(nullable = false)
