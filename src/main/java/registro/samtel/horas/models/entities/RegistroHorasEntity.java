351702637724bd833fb223df6a8e055b36766006
JoseRiv4s <navedasjose@gmail.com>
2025-04-11 12:48:37 -0500

    Refactor: Modificacion de las entidades RegistroHorasEntity y UsuarioEntity  con implementacion de validaciones de los campos

--- src/main/java/registro/samtel/horas/models/entities/RegistroHorasEntity.java
+++ src/main/java/registro/samtel/horas/models/entities/RegistroHorasEntity.java
@@ -2,11 +2,10 @@ package registro.samtel.horas.models.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Data
 @Table(name = "registro")
@@ -19,18 +18,19 @@ public class RegistroHorasEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @NotBlank(message = "TIENES QUE COLOCAR LA FECHA, GRACIAS!")
     private String fechaDeRegistro;
 
-
+    @NotBlank(message = "TIENES QUE COLOCAR LA HORA DE ENTRADA, GRACIAS!")
     private String horaEntrada;
 
-
+    @NotBlank(message = "TIENES QUE COLOCAR LA HORA DE SALIDA, GRACIAS!")
     private String horaSalida;
 
-
+    @NotBlank(message = "EL ESTADO NO PUEDE SER NULO, GRACIAS!")
     private Boolean estadoRegistro;
 
+    @NotBlank(message = "DEBES COLOCAR EL ID DEL USUARIO, GRACIAS!")
     @ManyToOne
     @JoinColumn(name = "usuario_id")
     private UsuarioEntity usuario;
--- src/main/java/registro/samtel/horas/models/entities/UsuarioEntity.java
+++ src/main/java/registro/samtel/horas/models/entities/UsuarioEntity.java
@@ -2,6 +2,9 @@ package registro.samtel.horas.models.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,18 +22,28 @@ public class UsuarioEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
 
+    @NotBlank(message = "TIENES QUE COLOCAR EL NOMBRE, GRACIAS!")
+    @Size(min = 3, max = 50, message = "El NOMBRE DE TENER ENTRE 3 Y 50 CARACTERES")
     @Column(nullable = false, length = 50)
     private  String nombre;
 
+    @NotBlank(message = "TIENES QUE COLOCAR EL CORREO, GRACIAS!")
+    @Email(message = "EL CORREO DEBE SER VALIDO, GRACIAS!")
+    @Size(max = 50, message = "El CORREO NO DEBER EXCEDER LOS 50 CARACTERES")
     @Column(nullable = false, length = 50, unique = true)
     private  String correo;
 
+    @NotBlank(message = "TIENES QUE COLOCARLE EL ROL, GRACIAS!")
     @Enumerated(EnumType.STRING) // Guardar enum como String en la base de datos
     @Column(nullable = false)
     private RolUsuario rol;
 
+
+    @NotBlank(message = "DEBES COLOCAR LA CONTRASEÑA, GRACIAS!")
+    @Size(min = 8, message = "lA CONTRASEÑA TIENE QUE TENER MINIMO 8 CARACTERES, GRACIAS!")
     private String password;
 
+    @NotBlank(message = "EL ESTADO NO PUEDE SER NULO, GRACIAS!")
     @Column(nullable = false)
     private Boolean estado;
 
